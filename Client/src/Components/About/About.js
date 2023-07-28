a6ec6dcd2c41f8d22ca320ac57a3de289df84a00
Daniel Karanja <sirkaranja12@gmail.com>
2023-07-28 14:20:35 +0300

    Navbar and About components updated

---
+++ Client/src/Components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import img1 from '../../assets/coursel.jpg'
+function About() {
+  return (
+   
+ <img src={img1}  className="img-fluid"  alt="Responsive image"   style={{ width: '100%'}}/>
+   
+   
+  )
+}
+
+export default About
