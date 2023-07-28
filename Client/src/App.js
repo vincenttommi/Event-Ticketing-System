fa5d4c01f95b68898230758d246f53ae4d66a088
Daniel Karanja <sirkaranja12@gmail.com>
2023-07-28 18:18:00 +0300

    done with work section

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import CollapsibleExample from './Components/Navbar/Navbar'
 import About from './Components/About/About'
+import Work from './Components/Works/Work'
 
 function App() {
   return (
    <>
    <CollapsibleExample />
    <About />
+   < Work />
    </>
   );
 }
