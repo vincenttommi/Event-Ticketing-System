191a5a1be6b8987cb5e3ed84e4684ce04fffd8ff
Daniel Karanja <sirkaranja12@gmail.com>
2023-07-29 13:13:58 +0300

    how it works section

--- Client/src/Components/Works/Work.js
+++ Client/src/Components/Works/Work.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import img1 from '../../assets/work.jpg'
 function Work() {
-  return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-    {/* Image */}
-    <img src={img1} alt="Your Image" style={{ width: '700px',height:'400px', marginRight: '20px' }} />
+  return ( <><h2 style={{marginLeft:'190px'}} >How it Works</h2>
+  <div style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={img1} alt="Your Image" style={{ width: '518px', height: '608px',borderRadius:'25px', marginRight: '20px', marginLeft:'90px' }} />
 
-    {/* Steps Information */}
-    <div>
-      <h2>How it Works</h2>
-      <ol>
-        <li>Step 1: Create</li>
-        <li>Step 2: Customize</li>
-        <li>Step 3: Promote</li>
-        <li>Step 4: Manage</li>
-        <li>Step 5: Receive</li>
-      </ol>
-    </div>
-  </div>
+      <div>
+
+          <ol>
+            <p><h1>1. Create</h1>Create an event and add a name, date, tickets and
+ description.</p>
+            <p><h1>2. Customize</h1>Add personality to your event page with event
+ details, images, videos, and more.</p>
+            <p><h1>3. Promote</h1>Share the event via social media with one click and
+ spread the word via email or in person.</p>
+            <p><h1>4. Manage</h1>Generate sales with early-bird discounts, coupons
+ and group ticketing features, and more.</p>
+            <p><h1>5. Receive</h1>Get paid directly to your bank account according
+ to your schedule.</p>
+          </ol>
+      </div>
+  </div></>
 
   )
 }
