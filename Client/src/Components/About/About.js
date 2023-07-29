1ddad277cbf3a6e3f10197459ecffb03ccdd8f1f
Daniel Karanja <sirkaranja12@gmail.com>
2023-07-29 15:31:17 +0300

    added some text

--- Client/src/Components/About/About.js
+++ Client/src/Components/About/About.js
@@ -12,9 +12,14 @@ function About() {
     <Container fluid>
       <div style={{ position: 'relative' }}>
         <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', zIndex: 1 }}>
-          <h2 style={{ paddingTop: '90px', color: '#fff', textAlign: 'center' }}>
+          <div style={{ paddingTop: '250px', color: '#fff', textAlign: 'center' }}>
+          <h2 >
             Event Ticketing Made Simple
           </h2>
+          <h4>Start Selling Tickets in 2 minutes</h4>
+          <button type="button" class="btn btn-success btn-lg">Learn More</button>
+          </div>
+         
         </div>
         <img src={img1} className="img-fluid" alt="Responsive image" style={{ width: '100%', height: 'auto', opacity: '0.8', zIndex: 0 }} />
         <h3 style={{ paddingTop: '50px', textAlign: 'center' }}>Events created by thousands of organizations around the globe</h3>
@@ -25,7 +30,7 @@ function About() {
           <img src={img5} className="img-fluid" alt="" style={{ maxWidth: '200px', margin: '10px' }} />
           <img src={img6} className="img-fluid" alt="" style={{ maxWidth: '200px', margin: '10px' }} />
         </div>
-        <div className="lowest" style={{ margin: '50px 0', height: '400px', background: '#f0f0f0', borderRadius: '25px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <div className="lowest" style={{ margin: '50px 0', height: '400px', background: '#f0f0f0', borderRadius: '25px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <div className="info" style={{ textAlign: 'center', padding: '20px' }}>
             <h2>Lowest fees in the industry</h2>
             <p style={{ fontSize: '20px' }}>
@@ -36,15 +41,21 @@ function About() {
             </p>
             <button type="button" className="btn btn-success">Learn More</button>
           </div>
-          <img src={img1} className="img-fluid" alt="" style={{ width: '200px', margin: '20px' }} />
+          <img src={img1} className="img-fluid" alt="" style={{ width: '200px', marginLeft: '20px' }} />
         </div>
-        <div className="powerful" style={{ margin: '50px 0', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-          <div className="info" style={{ textAlign: 'center', padding: '20px' }}>
-            <h1>Powerful Features in a Simple Dashboard</h1>
-            <p style={{ fontSize: '25px' }}>Need to set up recruiting events, Coupons, or custom form? We have got you covered.</p>
-          </div>
-          <img src={img1} className="img-fluid" alt="" style={{ width: '80%', maxWidth: '600px', borderRadius: '25px' }} />
-        </div>
+        <div className="powerful" style={{ margin: '50px 0', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+  <div className="info" style={{ padding: '20px' }}>
+    <h1>Powerful <br/>Features in a  Simple <br/>Dashboard</h1>
+    <p style={{ fontSize: '16px' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor <br/>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+     quis<br/> nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/> 
+     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore<br/>  eu 
+     fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+    non proident, sunt in culpa<br/> qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+  <img src={img1} className="img-fluid" alt="" style={{ width: '80%', maxWidth: '600px', borderRadius: '25px' }} />
+</div>
+
       </div>
     </Container>
   );
