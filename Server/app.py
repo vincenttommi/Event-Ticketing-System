8ef0ac70126b80fa5238ef3ea56b6de874934558
vincenttommi <vincenttommi@gmail.com>
2023-07-29 16:00:39 +0300

    initialising flask app

--- Server/app.py
+++ Server/app.py
@@ -1,1 +1,28 @@
-hfhfh
+#!usr/bin/env python3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate  import Migrate
+from models import db
+
+
+app = Flask(__name__)
+#creates a flask application instance called app
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#Disables automatic modification tracking in SQLAlchemy for a Flask app
+
+
+migrate = Migrate(app,db)
+#Migrate class sets up our migration using flask application instances and sqlalchemy instances 
+
+db.init_app(app) 
+#initialising  the application with database configuration 
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
+    
+
