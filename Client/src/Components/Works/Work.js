d7f7fdd508d943e576848d8cd818be4c9b6316c9
Daniel Karanja <sirkaranja12@gmail.com>
2023-07-29 14:11:47 +0300

    responsive media queries for responsiveness

--- Client/src/Components/Works/Work.js
+++ Client/src/Components/Works/Work.js
@@ -1,28 +1,25 @@
-import React from 'react'
-import img1 from '../../assets/work.jpg'
+import React from 'react';
+import img1 from '../../assets/work.jpg';
+import './works.css';
+
 function Work() {
-  return ( <><h2 style={{marginLeft:'190px'}} >How it Works</h2>
-  <div style={{ display: 'flex', alignItems: 'center' }}>
-      <img src={img1} alt="Your Image" style={{ width: '518px', height: '608px',borderRadius:'25px', marginRight: '20px', marginLeft:'90px' }} />
-
-      <div>
-
+  return (
+    <>
+      <h2 className="how-it-works-title">How it Works</h2>
+      <div className="how-it-works-container">
+        <img src={img1} alt="Your Image" className="works-image" />
+        <div className="works-description">
           <ol>
-            <p><h1>1. Create</h1>Create an event and add a name, date, tickets and
- description.</p>
-            <p><h1>2. Customize</h1>Add personality to your event page with event
- details, images, videos, and more.</p>
-            <p><h1>3. Promote</h1>Share the event via social media with one click and
- spread the word via email or in person.</p>
-            <p><h1>4. Manage</h1>Generate sales with early-bird discounts, coupons
- and group ticketing features, and more.</p>
-            <p><h1>5. Receive</h1>Get paid directly to your bank account according
- to your schedule.</p>
+            <li><h1> Create</h1>Create an event and add a name, date, tickets, and description.</li>
+            <li><h1> Customize</h1>Add personality to your event page with event details, images, videos, and more.</li>
+            <li><h1> Promote</h1>Share the event via social media with one click and spread the word via email or in person.</li>
+            <li><h1> Manage</h1>Generate sales with early-bird discounts, coupons, and group ticketing features, and more.</li>
+            <li><h1> Receive</h1>Get paid directly to your bank account according to your schedule.</li>
           </ol>
+        </div>
       </div>
-  </div></>
-
-  )
+    </>
+  );
 }
 
-export default Work
+export default Work;
