983140186ad7fc05367ce69caaad95483e757894
johnthairukamau <tkamau82@gmail.com>
2023-07-31 09:34:16 +0300

    Checkout

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,20 +1,22 @@
 // import Navbar from './Components/Navbar/Navbar';
 import Industry from './Components/Industry/industry';
 import './App.css';
-// import CollapsibleExample from './Components/Navbar/Navbar'
+import CollapsibleExample from './Components/Navbar/Navbar'
 import About from './Components/About/About'
 import Work from './Components/Works/Work'
 import Footer from './Components/Footer/footer'
 import Industry from './Components/Industry/industry';
 import SignupForm from './Components/Auth/SignUp'
 import LoginForm from './Components/Auth/Login'
+import Pricing from './Components/Pricing/pricing';
 
 function App() {
   return (
    <>
-   {/* <CollapsibleExample /> */}
+   <CollapsibleExample />
    <About />
    <Industry/>
+   <Pricing/>
    < Work />
    < Footer />
    {/* < LoginForm />
--- Client/src/Components/Navbar/Navbar.js
+++ Client/src/Components/Navbar/Navbar.js
@@ -1,28 +1,28 @@
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-// function CollapsibleExample() {
-//   return (
-//     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-//       <Container>
-//         <Navbar.Brand href="#home">EventMS</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//         <Navbar.Collapse id="responsive-navbar-nav">
-//           <Nav className="me-auto">
-//             <Nav.Link href="#features">Features</Nav.Link>
-//             <Nav.Link href="#Industry">Industry</Nav.Link>
-//             <Nav.Link href="#Events">Explore Events</Nav.Link>
-//             <Nav.Link href="#pricing">Pricing</Nav.Link>
-//           </Nav>
-//           <Nav>
-//             <Nav.Link href="{Login}">Login</Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// }
+function CollapsibleExample() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">EventMS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#Industry">Industry</Nav.Link>
+            <Nav.Link href="#Events">Explore Events</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="{Login}">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
-// export default CollapsibleExample;
+export default CollapsibleExample;
