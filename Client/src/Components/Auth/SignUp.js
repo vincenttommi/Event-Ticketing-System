d02c3583fdc99c4deb6d89ce7cab43f00d1be4c1
Daniel Karanja <sirkaranja12@gmail.com>
2023-08-09 15:36:04 +0300

    .

--- Client/src/Components/Auth/SignUp.js
+++ Client/src/Components/Auth/SignUp.js
@@ -9,11 +9,14 @@ const SignupForm = () => {
     password: '',
     confirmPassword: '',
     rememberMe: false,
+    phoneNumber: '',
+    role: 'attendee',
   });
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     const newValue = type === 'checkbox' ? checked : value;
+
     setFormData((prevData) => ({
       ...prevData,
       [name]: newValue,
@@ -29,76 +32,101 @@ const SignupForm = () => {
   return (
     <Container>
       <div className="signup-screen">
-      <div className="div">
-        <div className="overlap">
-          <h1 className="text-wrapper">EventMS</h1>
-        </div>
-        <div className="overlap-group">
-        <div className="signup-form">
-            <h2>Sign Up</h2>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="formName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  required
-                />
-              </Form.Group>
+        <div className="div">
+          <div className="overlap">
+            <h1 className="text-wrapper">EventMS</h1>
+          </div>
+          <div className="overlap-group">
+            <div className="signup-form">
+              <h2>Sign Up</h2>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="formName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                  />
+                </Form.Group>
 
-              <Form.Group controlId="formEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-              </Form.Group>
+                <Form.Group controlId="formEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </Form.Group>
 
-              <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                />
-              </Form.Group>
+                <Form.Group controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                  />
+                </Form.Group>
 
-              <Form.Group controlId="formConfirmPassword">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="confirmPassword"
-                  value={formData.confirmPassword}
-                  onChange={handleChange}
-                  required
-                />
-              </Form.Group>
+                <Form.Group controlId="formConfirmPassword">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="confirmPassword"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                    required
+                  />
+                </Form.Group>
 
-              <Form.Group controlId="formRememberMe">
-                <Form.Check
-                  type="checkbox"
-                  label="Remember me"
-                  name="rememberMe"
-                  checked={formData.rememberMe}
-                  onChange={handleChange}
-                />
-              </Form.Group><br></br>
+                <Form.Group controlId="formPhoneNumber">
+                  <Form.Label>Phone Number</Form.Label>
+                  <Form.Control
+                    type="tel"
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    onChange={handleChange}
+                    required
+                  />
+                </Form.Group>
 
-              <Button variant="primary" type="submit">
-                Sign Up
-              </Button>
-            </Form>
+                <Form.Group controlId="formRole">
+                  <Form.Label>Role</Form.Label>
+                  <Form.Control
+                    as="select"
+                    name="role"
+                    value={formData.role}
+                    onChange={handleChange}
+                    required
+                  >
+                    <option value="attendee">Attendee</option>
+                    <option value="organizer">Organizer</option>
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="formRememberMe">
+                  <Form.Check
+                    type="checkbox"
+                    label="Remember me"
+                    name="rememberMe"
+                    checked={formData.rememberMe}
+                    onChange={handleChange}
+                  />
+                </Form.Group><br />
+
+                <Button variant="primary" type="submit">
+                  Sign Up
+                </Button>
+              </Form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </Container>
   );
 };
