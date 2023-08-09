a402012fe6464e686100532e06e9f9d40bbf6b81
Daniel Karanja <sirkaranja12@gmail.com>
2023-08-09 12:58:40 +0300

    updated models file with role based

--- Server/models.py
+++ Server/models.py
@@ -1,123 +1,74 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.schema import CheckConstraint 
+from faker import Faker
 from flask_login import UserMixin
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
 db = SQLAlchemy(app)
+fake = Faker()
 
-
-class Admin(db.Model, UserMixin):
-    __tablename__ = 'admin'
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
-    role = db.Column(db.String, default='admin', nullable=False)  
-
-    # Implement UserMixin methods
-    def get_id(self):
-        return str(self.id)
-
-
-class Customer(db.Model, UserMixin):
-    __tablename__ = 'customers'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
-    phone_number = db.Column(db.String, nullable=False)
-    role = db.Column(db.String, default='customer', nullable=False)  
-
-    booked_events = db.relationship('BookedEvents', backref='customers', lazy=True)
-
-
-    # Implement UserMixin methods
-    def get_id(self):
-        return str(self.id)
-
-
-class Organiser(db.Model, UserMixin):
-    __tablename__ = 'organisers'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
-    industry = db.Column(db.String, nullable=False)
-    location = db.Column(db.String, nullable=False)
-    role = db.Column(db.String, default='organizer', nullable=False)  
-
-    __table_args__ = (
-        CheckConstraint(industry.in_(['Business', 'Political', 'Academic', 'Charity', 'Community', 'Entertainment', 'Sporting'])),
-    )
-
-    events = db.relationship('Events', backref='organisers', lazy=True)
-    payments = db.relationship('Payment', backref='organisers', lazy=True)
-    # Implement UserMixin methods
-    def get_id(self):
-        return str(self.id)
-
-
+    name = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), unique=True)
+    password = db.Column(db.String(), nullable=False)
+    phone_number = db.Column(db.String(), nullable=False)
+    role = db.Column(db.String(), nullable=False)
 
 class Events(db.Model):
-    __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    _price = db.Column('price', db.Float, nullable=False)
-    location = db.Column(db.String, nullable=False)
-    image = db.Column(db.String, nullable=True)
-    time = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
-    organiser_id = db.Column(db.Integer, db.ForeignKey('organisers.id'), nullable=False)
+    title = db.Column(db.String(), nullable=False)
+    location = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    event_category = db.Column(db.String(), nullable=False)
+    image = db.Column(db.String())
+    organizer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    event_time = db.Column(db.DateTime(), nullable=False)
+    ticket_levels = db.Column(db.String(), nullable=False)
+    vip_price = db.Column(db.Float, nullable=False)
+    regular_price = db.Column(db.Float, nullable=False)
+    early_bird_price = db.Column(db.Float, nullable=False)
     tickets_number = db.Column(db.Integer, default=0, nullable=False)
-    tickets_status = db.Column(db.String, default='0', nullable=False)
-    ticket_level = db.Column(db.String, nullable=False)  # ticket_level field
+    remaining_tickets = db.Column(db.Integer, default=0, nullable=False)
 
-    def get_price(self):
-        return self._price
-
-    def set_price(self, new_price):
-        self._price = new_price
-
-    price = property(get_price, set_price)
-
-    def get_price_by_ticket_level(self):
-        if self.ticket_level == 'VIP':
-            return self._price * 2
-        elif self.ticket_level == 'Early Bird':
-            return self._price * 1.5
+    def get_ticket_price(self, ticket_level):
+        if ticket_level == "VIP":
+            return self.vip_price
+        elif ticket_level == "Regular":
+            return self.regular_price
+        elif ticket_level == "Early Bird":
+            return self.early_bird_price
         else:
-            return self._price
-
-    booked_events = db.relationship('BookedEvents', backref='events', lazy=True)
-
-    revenue = db.relationship('Revenue', backref='events', lazy=True)
+            raise ValueError("Invalid ticket level")
 
 
 class BookedEvents(db.Model):
-    __tablename__ = 'booked_events'
     id = db.Column(db.Integer, primary_key=True)
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
-    event_title = db.Column(db.String, nullable=False)
-    event_location = db.Column(db.String, nullable=False)
-    ticket_level = db.Column(db.String, nullable=False)
-    event = db.relationship('Events', backref='booked_events_list', lazy=True)
-
-
-class Payment(db.Model):
-    __tablename__ = 'payments'
-    id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
-    amount = db.Column(db.Integer, nullable=False)
-    organiser_id = db.Column(db.Integer, db.ForeignKey('organisers.id'), nullable=False)
-
-
+    event_title = db.Column(db.String(), nullable=False)
+    event_location = db.Column(db.String(), nullable=False)
+    ticket_level = db.Column(db.String(), nullable=False)
+    attendee_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 class Revenue(db.Model):
-    __tablename__ = 'revenues'
     id = db.Column(db.Integer, primary_key=True)
-    total_amount = db.Column(db.String, nullable=False)
-    tickets_sold = db.Column(db.Integer, nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    total_amount = db.Column(db.Float, nullable=False)
+    organizer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    @property
+    def tickets_sold(self):
+        return self.event.tickets_number - self.event.remaining_tickets
+
+    @classmethod
+    def calculate_total_amount(cls, organizer_id):
+        events = Events.query.filter_by(organizer_id=organizer_id).all()
+        total = 0.0
+        for event in events:
+            total += event.vip_price + event.regular_price + event.early_bird_price
+        return total
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all() 
+    app.run()
